a3a63beb0977dc5170138089f218fb8cfd8551f7
Prakriti123456 <93470873+Prakriti123456@users.noreply.github.com>
2023-09-07 18:47:42 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+noseX=0;
+noseY=0;
+difference=0;
+rightWX=0;
+leftWX=0;
+
 function setup(){
     video=createCapture(VIDEO);
     video.size(550,500);
@@ -11,15 +17,33 @@ function setup(){
     
     function draw(){
         background('#969A97');
+        textsize(difference);
+        fill('#FFE787');
+        text('Prakriti Jaiswal',50,400);
     }
     
     function gotPoses(results){
         if (results.length > 0)
         {
             console.log(results);
+            noseX=results[0].pose.nose.x;
+            noseY=results[0].pose.nose.y;
+            console.log("noseX = "+ noseX+"noseY = "+noseY);
+    
+            leftWX=results[0].pose.leftWrist.x;
+            rightWX=results[0].pose.rightWrist.x;
+            difference=floor(leftWX-rightWX);
+    
+            console.log("leftWristX="+leftWX+"rightWristX="+rightWX+"difference="+difference);
         }
     }
     
     function modelLoaded(){
         console.log('PoseNet Is Initialized');
     }
+
+    
+
+
+
+
